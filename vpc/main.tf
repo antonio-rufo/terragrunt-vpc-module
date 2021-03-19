5cd00dc60a2e5ea80696a25707832e052850db92
Antonio Cuenco <antonio.cuenco@qantasloyalty.com>
2021-03-19 15:37:03 +1100

    Terragrunt VPC module

---
+++ vpc/main.tf
@@ -0,0 +1,47 @@
+terraform {
+  # The configuration for this backend will be filled in by Terragrunt
+  backend "s3" {}
+
+  # Live modules pin exact Terraform version; generic modules let consumers pin the version.
+  # The latest version of Terragrunt (v0.19.0 and above) requires Terraform 0.12.0 or above.
+  required_version = "> 0.12.4"
+}
+
+###############################################################################
+# Providers
+###############################################################################
+provider "aws" {
+  region = var.region
+
+  assume_role {
+    role_arn     = "arn:aws:iam::${var.aws_account_id}:role/assumed-terraform"
+    session_name = "QL_OPS_TERRAFORM_SESSION"
+    external_id  = "QL_TECH_OPS_TF"
+  }
+}
+
+locals {
+  tags = {
+    Environment = var.environment
+  }
+}
+
+data "aws_availability_zones" "available" {
+}
+
+###############################################################################
+# Base Network
+###############################################################################
+module "base_network" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.64.0"
+
+  name                 = var.vpc_name
+  cidr                 = var.cidr_range
+  azs                  = data.aws_availability_zones.available.names
+  private_subnets      = var.public_cidr_ranges
+  public_subnets       = var.private_cidr_ranges
+  enable_nat_gateway   = true
+  enable_dns_hostnames = true
+  tags                 = local.tags
+}
